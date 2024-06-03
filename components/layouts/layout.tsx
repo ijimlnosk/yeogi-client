f7481c9025c26712229e98c807c0d13631ebd5dd
kiminn <134191815+kiminn@users.noreply.github.com>
2024-06-04 01:06:21 +0900

    YG-25 #close #comment 🟢 feat : header complete

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -4,11 +4,11 @@ import Footer from "./footer"
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Header />
-            <main>{children}</main>
+            <main className="flex-1 mt-[90px]">{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
 
