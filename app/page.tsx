397e44b8dfacb13db3983179b94a1652b57aedb6
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-04 05:25:06 +0900

    YG-27 #close  🟢 버튼 공용 컴포넌트 생성 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+"use client"
+
+import Button from "@/components/commons/button"
+import { useState } from "react"
+
 export default function Home() {
+    const [isActive, setIsActive] = useState(false)
+
+    const handleToggleActive = () => {
+        setIsActive(prev => !prev)
+    }
     return (
         <div className="p-4">
             <h1 className="font-myeongjo text-title text-BRAND-50">
@@ -10,6 +20,17 @@ export default function Home() {
             <div className="mt-4 p-4 bg-GREY-10">
                 <h2 className="text-subTitle text-ACCENT-orange">Subtitle with ACCENT orange color.</h2>
                 <p className="text-sm text-GREY-70">Some description with GREY 70 color.</p>
+            </div>
+            <div>
+                <Button>기본 버튼</Button>
+                <Button
+                    background={isActive ? "brand30" : "gray20"}
+                    textColor={isActive ? "white" : "black"}
+                    onClick={handleToggleActive}
+                    className="w-[190px] h-[54px]"
+                >
+                    textBox_md
+                </Button>
             </div>
         </div>
     )
---
+++ components/commons/button.tsx
@@ -0,0 +1,89 @@
+import { ButtonHTMLAttributes, ReactNode } from "react"
+import { VariantProps, cva } from "class-variance-authority"
+import { cn } from "@/utils/tailwindUtils"
+
+/**
+ * 버튼 스타일 정의
+ * author: Gang
+ * @param {object}
+ * @returns {string}
+ */
+
+const buttonStyle = cva("flex items-center justify-center px-4 py-2 active:scale-95", {
+    variants: {
+        rounded: {
+            sm: "rounded-sm",
+            md: "rounded-md",
+            lg: "rounded-lg",
+        },
+        background: {
+            gray20: "bg-GREY-20",
+            black: "bg-SYSTEM-black",
+            white: "bg-SYSTEM-white",
+            brand30: "bg-BRAND-30",
+            brand50: "bg-BRAND-50",
+        },
+        textColor: {
+            black: "text-SYSTEM-black",
+            brand50: "text-BRAND-50",
+            white: "text-SYSTEM-white",
+        },
+    },
+    defaultVariants: {
+        rounded: "md",
+        background: "gray20",
+        textColor: "black",
+    },
+})
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+    VariantProps<typeof buttonStyle> & {
+        children?: ReactNode
+        isActive?: boolean
+    }
+/**
+ * 공용 버튼 컴포넌트
+ * author: Gang
+ * @param {ButtonProps} props
+ * @returns {JSX.Element}
+ *
+ * @example
+ * // 기본 사용법
+ * <Button>기본 버튼</Button>
+ *
+ * @example
+ * // 라운드 크기, 배경색, 텍스트 색상을 지정하여 사용
+ * <Button rounded="lg" background="brand50" textColor="white">커스텀 버튼</Button>
+ *
+ */
+const Button = ({
+    type = "button",
+    className,
+    rounded,
+    background,
+    textColor,
+    onClick,
+    children,
+    isActive = false,
+    ...props
+}: ButtonProps) => {
+    return (
+        <button
+            type={type}
+            className={cn(
+                buttonStyle({
+                    rounded,
+                    background: isActive ? background : background,
+                    textColor: isActive ? textColor : textColor,
+                }),
+                className,
+            )}
+            onClick={onClick}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button
