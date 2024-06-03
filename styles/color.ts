c255af734b8c9d2a138f36131728341528cb2d91
55555-Jyeon <jyeon380516@gmail.com>
2024-06-03 15:41:11 +0900

    디자인 토큰: 색상과 폰트사이즈 설정

---
+++ styles/color.ts
@@ -0,0 +1,26 @@
+export const COLORS = {
+    SYSTEM: {
+        black: "#0c0c0c",
+        white: "#fff",
+        beige: "#f0f0f0",
+    },
+    GREY: {
+        10: "#eee",
+        20: "#ddd",
+        30: "#c9c9c9",
+        50: "#aaa",
+        70: "#919191",
+        80: "#6c6c6c",
+    },
+    BRAND: {
+        10 : "#C0DCAB",
+        30 : "#90B773",
+        50 : "#658F47",
+        70 : "#395143",
+        90 : "#162623",
+    },
+    ACCENT: {
+        orange: "#F07D13",
+        coral: "#EF7974",
+    }
+} as const;
---
+++ styles/font.ts
@@ -0,0 +1,13 @@
+export const FONT_SIZE= {
+    title: "144px",
+    subTitle: "56px",
+    xxl: "48px",
+	xl: "32px", 
+	lg: "26px", 
+	bg: "24px", 
+	md: "20px", 
+	sm: "18px", 
+	xs: "16px", 
+	xxs: "15px", 
+}as const;
+
