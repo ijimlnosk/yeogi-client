db476d55fab208cbad9d4baa5789d2d8f7576b84
55555-Jyeon <jyeon380516@gmail.com>
2024-06-04 03:31:51 +0900

    YG-21 📂 함수마다 JS DOCS 작성

--- components/commons/pagination.tsx
+++ components/commons/pagination.tsx
@@ -5,11 +5,27 @@ import clsx from "clsx"
 import Link from "next/link"
 import { usePathname, useSearchParams } from "next/navigation"
 
+/**
+ * @function Pagination
+ * @param totalPages 전체 페이지
+ * @const pathname 현재 경로
+ * @const searchParams URL의 검색 매개변수
+ * @const currentPage URL의 page 매개변수에서 가져옴 (기본값 = 1)
+ * @const allPages generatePagination 함수를 사용하여 현재 페이지와 전체 페이지 수에 기반하여 표시할 페이지 목록을 생성
+ * @const prevDisabled 현재 페이지가 5 이하일 경우 true로 설정하여 이전 버튼을 비활성화
+ * @const nextDisabled 다음 그룹이 없을 경우 true로 설정하여 다음 버튼을 비활성화
+ * @returns 화면에 렌더될 숫자 버튼 배열과 이전, 다음 버튼을 반환
+ */
 export default function Pagination({ totalPages }: { totalPages: number }) {
     const pathname = usePathname()
     const searchParams = useSearchParams()
     const currentPage = Number(searchParams.get("page")) || 1
 
+    /**
+     * @function createPageURL URL 생성 함수
+     * @param pageNumber 페이지 번호
+     * @returns 해당 페이지로 이동할 URL을 생성해 반환
+     */
     const createPageURL = (pageNumber: number | string) => {
         const params = new URLSearchParams(searchParams)
         params.set("page", pageNumber.toString())
@@ -44,6 +60,14 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     )
 }
 
+/**
+ * @function PaginationNumber 각 페이지 번호를 렌더링
+ * @param page 렌더링할 페이지 번호
+ * @param href 클릭 시 이동할 URL
+ * @param isActive 현재 클릭된 페이지인지의 여부
+ * @const className clsx를 사용하여 isActive 상태에 따라 CSS 클래스를 적용
+ * @returns isActive 여부에 따라 해당 스타일이 적용된 숫자 버튼을 반환
+ */
 function PaginationNumber({ page, href, isActive }: { page: number | string; href: string; isActive: boolean }) {
     const className = clsx("flex w-[25px] h-[25px] items-center justify-center mx-1.5", {
         "z-10 border-[1px] border-SYSTEM-black rounded-full text-SYSTEM-black": isActive,
