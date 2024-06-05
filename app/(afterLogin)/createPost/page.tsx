c52c357c73bdd704f85e37a494a9caf13c7cfb96
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-05 17:16:45 +0900

    YG-33 #close #comment 💡refactor: 문구를 존댓말로 수정했습니다

--- app/(afterLogin)/createPost/page.tsx
+++ app/(afterLogin)/createPost/page.tsx
@@ -28,7 +28,7 @@ const Page = () => {
             <div className="w-[1040px] h-[704px] flex flex-col items-center justify-center">
                 <div className="flex flex-col items-center justify-center pb-[40px]">
                     <p className="font-myeongjo text-xl">
-                        여기는 기록할 수 있는 <span className="text-BRAND-50">두 가지</span> 방법을 제공해!
+                        여기는 기록할 수 있는 <span className="text-BRAND-50">두 가지</span> 방법을 제공합니다!
                     </p>
                     <p className="font-myeongjo text-xl">본인의 기록 스타일을 선택하세요!</p>
                 </div>
