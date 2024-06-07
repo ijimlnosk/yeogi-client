a5d172818e1fceec052db96f9da7c322fc14fd1b
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-07 23:48:57 +0900

    YG-44 #close #comment 💡refactor: overlay type분리 및 docs 작성

--- components/commons/overlay.tsx
+++ components/commons/overlay.tsx
@@ -2,25 +2,19 @@
 
 import clsx from "clsx"
 import Image from "next/image"
-import { ReactNode, useEffect } from "react"
-
-type OverlayProps = {
-    isOpen: boolean
-    onClick: () => void
-    children: ReactNode
-    widthCss?: string
-    heightCss?: string
-    text?: string
-    imageUrl?: string
-    textColor?: string
-}
+import { useEffect } from "react"
+import { OverlayProps } from "./type"
 
 /**
  * Overlay Component
- * author: Gang
+ * @author: Gang
  * @param {boolean} isOpen - 오버레이의 표시 여부 제어
- * @param {Function} props.onClose - 오버레이를 닫을 때 호출되는 콜백함수 닫기 or 오버레이 바깥 클릭 시 닫힘
+ * @param {Function} props.onClick - 오버레이를 닫을 때 호출되는 콜백함수 닫기 or 오버레이 바깥 클릭 시 닫힘
  * @param {ReactNode} props.children - 오버레이 내부에 표시될 내용
+ * @param {string} props.text - 닫기 버튼에 표시될 텍스트
+ * @param {string} props.imageUrl - 오버레이에 표시될 이미지 URL
+ * @param {string} props.textColor - 닫기 버튼 텍스트 색상
+ *
  */
 
 const Overlay = ({ isOpen, onClick, children, text, imageUrl, textColor }: OverlayProps) => {
