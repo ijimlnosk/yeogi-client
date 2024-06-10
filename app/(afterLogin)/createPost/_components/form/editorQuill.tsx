8c026fc2768ac5ff7958fcaa7283ce54da9836d9
55555-Jyeon <jyeon380516@gmail.com>
2024-06-11 02:39:55 +0900

    YG-40 🟢 memo 삭제 버튼 임의로 추가 완료

--- app/(afterLogin)/createPost/_components/form/editorQuill.tsx
+++ app/(afterLogin)/createPost/_components/form/editorQuill.tsx
@@ -2,15 +2,33 @@
 
 import dynamic from "next/dynamic"
 import "react-quill/dist/quill.snow.css"
-import React from "react"
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false })
 
-export const QuillEditor = () => {
+export type QuillEditorProps = {
+    index: number
+    handleDeleteQuillEditor?: (index: number) => void
+}
+
+export const QuillEditor = ({ index, handleDeleteQuillEditor }: QuillEditorProps) => {
+    const handleDeleteClick = (index: number) => {
+        if (handleDeleteQuillEditor) handleDeleteQuillEditor(index)
+    }
+
     return (
-        <div className="quill-editor-wrapper">
+        <div className="quill-editor-wrapper my-4">
+            <div className="relative top-10 w-[940px] flex justify-end">
+                {index !== -1 && (
+                    <button
+                        onClick={() => handleDeleteClick(index)}
+                        className="w-[30px] h-[30px] bg-SYSTEM-white text-GREY-50 rounded-full"
+                    >
+                        x
+                    </button>
+                )}
+            </div>
             <ReactQuill
-                className="quill-editor bg-white rounded-lg shadow-lg"
+                className="relative quill-editor bg-white rounded-lg shadow-lg"
                 modules={{
                     toolbar: [
                         [{ font: [] }],
--- app/(afterLogin)/memoForm/page.tsx
+++ app/(afterLogin)/memoForm/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import ThumbnailUploader from "../createPost/_components/thumbnailUploader"
 import Overlay from "@/components/commons/overlay"
 import PreviewPostCard from "../createPost/_components/previewPostcard"
@@ -16,10 +16,22 @@ const Page = () => {
     const [isOverlayOpen, setIsOverlayOpen] = useState(false)
     const [isPreviewVisible, setIsPreviewVisible] = useState(false)
     const [selectedImage, setSelectedImage] = useState<string | null>(null)
+    const [quillEditors, setQuillEditors] = useState<number[]>([])
+    const quillEditorRefs = useRef<(HTMLElement | null)[]>([])
 
     const handleComplete = (selectedImage: string | null) => {
         setSelectedImage(selectedImage)
         setIsPreviewVisible(true)
+    }
+
+    const handleAddMemoClick = () => {
+        setQuillEditors([...quillEditors, quillEditors.length])
+    }
+
+    const handleDeleteQuillEditor = (index: number) => {
+        const updatedEditors = [...quillEditors]
+        updatedEditors.splice(index, 1)
+        setQuillEditors(updatedEditors)
     }
 
     return (
@@ -41,10 +53,18 @@ const Page = () => {
                     <PreviewPostCard selectedImage={selectedImage} />
                 )}
             </Overlay>
-            <div className="w-[900px] h-full font-pretendard ">
+            <div className="w-[900px] h-full font-pretendard">
                 <FormInputs formText={"간단하게 "} />
-                <QuillEditor />
-                <div className="w-[900px] h-[48px] my-[30px] flex flex-row justify-center items-center rounded-[61px] bg-SYSTEM-beige border-[1px] border-BRAND-50">
+                <QuillEditor index={-1} />
+                {quillEditors.map((id, index) => (
+                    <div key={id}>
+                        <QuillEditor index={index} handleDeleteQuillEditor={() => handleDeleteQuillEditor(index)} />
+                    </div>
+                ))}
+                <div
+                    onClick={handleAddMemoClick}
+                    className="w-[900px] h-[48px] my-[30px] flex flex-row justify-center items-center rounded-[61px] bg-SYSTEM-beige border-[1px] border-BRAND-50"
+                >
                     <Image width={24} height={24} src={AddMemoIcon} alt="add memo icon" />
                     <p className="text-sm text-BRAND-50 mx-2">메모 추가하기</p>
                 </div>
