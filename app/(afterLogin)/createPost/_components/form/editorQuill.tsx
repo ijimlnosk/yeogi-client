e39f91bc623e816849faa06aa5433831e9a2b940
55555-Jyeon <jyeon380516@gmail.com>
2024-06-12 01:41:59 +0900

    YG-4 #close #comment 🎁 editor 스타일 변경

--- app/(afterLogin)/createPost/_components/form/editorQuill.tsx
+++ app/(afterLogin)/createPost/_components/form/editorQuill.tsx
@@ -31,13 +31,12 @@ export const QuillEditor = ({ index, handleDeleteQuillEditor }: QuillEditorProps
                 className="relative quill-editor bg-SYSTEM-white rounded-lg shadow-lg"
                 modules={{
                     toolbar: [
-                        [{ font: [] }],
+                        [{ font: ["font-pretendard", "font-myeongjo"] }],
                         [{ header: [1, 2, 3, 4, 5, 6] }, { header: false }],
                         [{ list: "ordered" }, { list: "bullet" }],
                         ["bold", "italic", "underline", "strike", "blockquote"],
                         [{ align: [] }],
                         ["link", "image"],
-                        [{ color: [] }, { background: [] }],
                         ["clean"],
                     ],
                 }}
