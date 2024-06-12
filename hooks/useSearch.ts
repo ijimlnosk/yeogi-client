14782ea17e509f797670dddf68c4ae1a44fba371
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-12 21:32:27 +0900

    YG-56 🟢feat: docs 생성

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -4,6 +4,14 @@ import { useEffect, useState } from "react"
 import { Post } from "./type"
 import { chosungIncludes } from "es-hangul"
 
+/**
+ * 주어진 게시글 배열과 검색어를 사용하여 검색 결과를 반환한다
+ *
+ * @param {Post[]} posts - 검색할 게시글 배열
+ * @param {string} searchTerm - 검색어 문자열
+ * @returns {Post[]} 필터링된 게시글 배열
+ */
+
 const useSearch = (posts: Post[], searchTerm: string) => {
     const [results, setResults] = useState<Post[]>(posts)
 
