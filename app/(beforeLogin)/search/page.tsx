703b0b6a4a0fedbfb15008de8af3993cd973fa1e
55555-Jyeon <jyeon380516@gmail.com>
2024-06-13 22:49:46 +0900

    YG-63 🔥 #close  #comment build error 해결 완료

--- app/(beforeLogin)/search/page.tsx
+++ app/(beforeLogin)/search/page.tsx
@@ -7,9 +7,9 @@ import { filterPosts } from "@/utils/filterPosts"
 import { Post } from "@/utils/type"
 import dynamic from "next/dynamic"
 import { useSearchParams } from "next/navigation"
-import { Suspense, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
-const SearchResults = dynamic(() => import("@/components/commons/searchResults"))
+const SearchResults = dynamic(() => import("@/components/commons/searchResults"), { ssr: false })
 
 const SearchPage = () => {
     const searchParams = useSearchParams()
@@ -24,37 +24,37 @@ const SearchPage = () => {
     }, [searchKeyword])
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="px-[120px] py-10">
-                <div className="flex flex-row justify-start items-center">
-                    <div className="text-bg text-GREY-80 font-medium">
-                        {posts.length > 0 ? (
-                            <>
-                                <span className="text-BRAND-50">{searchKeyword}</span>과 관련된 총
-                                <span className="text-BRAND-50">{posts.length}</span>의 검색 결과를 찾았어요!
-                                <span className="ml-10">
-                                    <SortDropdown />
-                                </span>
-                            </>
-                        ) : (
-                            <>
-                                <p>
-                                    <span className="text-BRAND-50">{searchKeyword}</span>과 관련된 검색 결과를 찾을 수
-                                    없어요!
-                                </p>
-                                <p>올바른 검색어를 입력하셨나요?</p>
-                            </>
-                        )}
-                    </div>
+        <div className="px-[120px] py-10">
+            <div className="flex flex-row justify-start items-center">
+                <div className="text-bg text-GREY-80 font-medium">
+                    {posts.length > 0 ? (
+                        <>
+                            <span className="text-BRAND-50">{searchKeyword}</span>과 관련된 총
+                            <span className="text-BRAND-50">{posts.length}</span>의 검색 결과를 찾았어요!
+                            <span className="ml-10">
+                                <SortDropdown />
+                            </span>
+                        </>
+                    ) : (
+                        <>
+                            <p>
+                                <span className="text-BRAND-50">{searchKeyword}</span>과 관련된 검색 결과를 찾을 수
+                                없어요!
+                            </p>
+                            <p>올바른 검색어를 입력하셨나요?</p>
+                        </>
+                    )}
                 </div>
-                {posts.length > 0 && (
-                    <>
-                        <SearchResults posts={posts} />
-                        <Pagination totalPages={Math.ceil(posts.length) / 8} />
-                    </>
-                )}
             </div>
-        </Suspense>
+            {posts.length > 0 && (
+                <>
+                    <SearchResults posts={posts} />
+                    <Pagination totalPages={Math.ceil(posts.length) / 8} />
+                </>
+            )}
+        </div>
     )
 }
-export default SearchPage
+
+const CSRSearchPage = dynamic(() => Promise.resolve(SearchPage), { ssr: false })
+export default CSRSearchPage
