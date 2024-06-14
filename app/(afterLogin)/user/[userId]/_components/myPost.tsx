af5a8cc9f96bf9c7016f392b5d08bd7216417104
kiminn <134191815+kiminn@users.noreply.github.com>
2024-06-15 02:32:13 +0900

    YG-70 #open #comment  🟢 feat :  my-page-merge

---
+++ app/(afterLogin)/user/[userId]/_components/myPost.tsx
@@ -0,0 +1,35 @@
+import PostCard from "@/components/commons/postCard"
+import TempImage from "@/public/images/sampleThumbnail.svg"
+import { Post } from "@/utils/type"
+
+const MyPost = ({ posts }: { posts: Post[] }) => {
+    if (posts && posts.length > 0)
+        return (
+            <div className="w-full flex flex-col items-center justify-center">
+                <div className="w-[1680px] px-5 pb-5">
+                    <p className="font-semibold text-xl">
+                        <span className="text-BRAND-50">Kimi</span>님의 기록
+                    </p>
+                </div>
+                <div className="w-[1680px] grid grid-cols-4 gap-4 pt-6 pb-20">
+                    {posts.map(post => (
+                        <div key={post.postId} className="w-fit p-2">
+                            <PostCard
+                                key={post.postId}
+                                post_id={post.postId}
+                                user_profile={post.userProfile}
+                                thumbnail={TempImage}
+                                title={post.title}
+                                continent={post.continent}
+                                user_nickname={post.nickName}
+                                created_At={new Date(post.createdAt)}
+                                commentCount={post.commentCount}
+                                likeCount={post.likeCount}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+}
+export default MyPost
