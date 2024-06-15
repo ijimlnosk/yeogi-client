063465538222c1c646a8499a290c43c4653afc1c
55555-Jyeon <jyeon380516@gmail.com>
2024-06-15 17:33:20 +0900

    YG-00 🔥 query-client error 해결

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,15 +1,12 @@
 import Header from "./header"
 import Footer from "./footer"
-import { ReactQueryProvider } from "../queryProvider"
 import { ReactNode } from "react"
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
-            <ReactQueryProvider>
-                <main className="flex-1 mt-[90px]">{children}</main>
-            </ReactQueryProvider>
+            <main className="flex-1 mt-[90px]">{children}</main>
             <Footer />
         </div>
     )
