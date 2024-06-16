739f0001ba1cc04b88e1fa32f42020fb56555d51
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-16 18:42:20 +0900

    YG-73 💡refactor: #close #comment 댓글 최신순 정렬

--- app/(afterLogin)/detailPost/[postId]/_components/commentBox.tsx
+++ app/(afterLogin)/detailPost/[postId]/_components/commentBox.tsx
@@ -2,11 +2,12 @@ import Comment from "@/components/commons/comment"
 import { CommentBoxProps } from "./type"
 
 const CommentBox = ({ comments }: CommentBoxProps) => {
+    const sortComment = comments.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
     return (
         <div className="pt-[30px]">
             {comments.length > 0 ? (
                 <div className="w-full flex items-center justify-center flex-col">
-                    {comments.map(commentWithLike => (
+                    {sortComment.map(commentWithLike => (
                         <div key={commentWithLike.id} className="flex flex-col items-center justify-center pb-[80px]">
                             <Comment
                                 content={commentWithLike.content}
