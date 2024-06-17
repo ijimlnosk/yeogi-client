0b2f30dd215ab30ef29859b18d9f1d9666fb07b2
55555-Jyeon <jyeon380516@gmail.com>
2024-06-17 18:10:13 +0900

    YG-74 💡 form의 공통 함수 util로 분리

--- utils/commonFormUtils.ts
+++ utils/commonFormUtils.ts
@@ -1,5 +1,10 @@
 import { ResizeImageProps, resizeBase64Image } from "@/utils/resizeImage"
 
+/**
+ * @function processContentImages 이미지 태그를 추출하여 base64 형식의 이미지를 리사이징
+ * @param {string} content - 리사이징할 이미지가 포함된 HTML 콘텐츠
+ * @returns {Promise<string>} - 리사이징된 HTML 콘텐츠 (base64 이미지가 리사이징되어 포함됨)
+ */
 export const processContentImages = async (content: string): Promise<string> => {
     // 이미지 태그를 추출하여 각 이미지에 대해 리사이징을 수행
     const imgTags = content.match(/<img[^>]+>/g)
@@ -32,4 +37,24 @@ export const processContentImages = async (content: string): Promise<string> => 
     }
 
     return processedContent
+}
+
+import { createPostTemplate } from "@/apis/type"
+
+/**
+ * @function useFormHandler 입력 필드 변경 핸들러
+ * @template K 입력 필드 키
+ * @param {createPostTemplate} formData 현재 폼 데이터 상태
+ * @param {React.Dispatch<React.SetStateAction<createPostTemplate>>} setFormData 폼 데이터 상태를 업데이트할 세터 함수
+ * @returns {(field: K, value: createPostTemplate[K]) => void} 입력 필드 변경 핸들러 함수
+ */
+export const useFormHandler = (
+    formData: createPostTemplate,
+    setFormData: (data: React.SetStateAction<createPostTemplate>) => void,
+) => {
+    const handleInputChange = <K extends keyof createPostTemplate>(field: K, value: createPostTemplate[K]) => {
+        setFormData(prevData => ({ ...prevData, [field]: value }))
+    }
+
+    return handleInputChange
 }
