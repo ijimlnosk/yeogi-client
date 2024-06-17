71b2bb7ec4ad256974e943bd7b6a8ea71c178d1b
55555-Jyeon <jyeon380516@gmail.com>
2024-06-17 20:18:34 +0900

    YG-74 🎁 tailwindCSS 클래스명으로 변경

--- app/(afterLogin)/detailPost/[postId]/_components/memoFormDetail.tsx
+++ app/(afterLogin)/detailPost/[postId]/_components/memoFormDetail.tsx
@@ -2,9 +2,9 @@ import { PostDetailProps } from "./type"
 
 const MemoFormDetail = ({ title, author, created_At, destination, travel_range, shortPosts }: PostDetailProps) => {
     return (
-        <div className="w-[1000px] bg-comment-pattern bg-SYSTEM-white h-auto flex items-center justify-center flex-col border-2 border-GREY-50 rounded-[16px] pt-[20px] pb-[20px]">
-            <div className="w-[960px] border-2 border-GREY-30 rounded-[16px] p-[20px] ">
-                <div className="w-full flex justify-between border-t-2 border-b-2 p-2 border-GREY-30">
+        <div className="w-[1000px] bg-comment-pattern bg-SYSTEM-white h-auto flex items-center justify-center flex-col border-2 border-GREY-50 rounded-[16px] p-5">
+            <div className="w-[960px] border-2 border-GREY-30 rounded-[16px] p-5 ">
+                <div className="w-full flex justify-between border-y-2 p-2 border-GREY-30">
                     <p>
                         게시일 : <span className="font-bold">{created_At}</span>
                     </p>
@@ -13,10 +13,10 @@ const MemoFormDetail = ({ title, author, created_At, destination, travel_range, 
                     </p>
                 </div>
                 <div className="pt-[20px]">
-                    <div className="flex items-center justify-center pt-[20px] pb-[20px] border-t-2 border-b-2 border-BRAND-50">
+                    <div className="flex items-center justify-center p-5 border-y-2 border-BRAND-50">
                         <p className="text-xxl">{title}</p>
                     </div>
-                    <div className="mt-[10px] pt-[10px] pb-[10px] flex flex-row justify-between items-center  border-t-2 border-GREY-30">
+                    <div className="mt-2.5 p-2.5 flex flex-row justify-between items-center  border-t-2 border-GREY-30">
                         <div className="w-[94px]">
                             <p className="flex justify-between">
                                 여행지 <span className="text-BRAND-50 font-bold">{destination}</span>
