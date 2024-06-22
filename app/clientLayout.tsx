43df50896e42be0c9b5770281feed2c117ef956e
hayoung <wendy4332@naver.com>
2024-06-23 03:03:55 +0900

    YG-82 💡 refactor : 필요없는 파일 / 코드 삭제

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -3,7 +3,7 @@
 import useModalStore from "@/libs/modalStore"
 import Overlay from "@/components/commons/overlay"
 import { ReactNode } from "react"
-import SocialSigninForm from "./(beforeLogin)/_auth/signin/socialSigninForm"
+import AuthForm from "./auth/page"
 
 /**
  * 전역 상태로 로그인 오버레이 제어
@@ -17,7 +17,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
     return (
         <>
             <Overlay isOpen={showLoginModal} onClick={closeModal} rounded="lg">
-                {showLoginModal && <SocialSigninForm />}
+                {showLoginModal && <AuthForm />}
             </Overlay>
             {children}
         </>
