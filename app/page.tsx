c7f47b61686d341665331556325080aefd8bd740
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-26 21:08:23 +0900

    YG-95 💡refactor: #close #comment 불필요한 로직 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
 "use client"
 
-import { useState } from "react"
 import MainDesc from "./_components/mainDesc"
 import MainIntro from "./_components/mainIntro"
 import MainPosts from "./_components/mainPosts"
-import AddressAutoComplete from "./(afterLogin)/createPost/_components/addressSearch/addressAutoComplete"
-import MapDiv from "./(afterLogin)/detailPost/[postId]/_components/MapDiv"
-import { Location } from "./(afterLogin)/detailPost/[postId]/_components/type"
 
 const Home = () => {
-    const [location, setLocation] = useState<Location | null>(null)
-
-    const handleSelect = (location: Location) => {
-        setLocation(location)
-    }
-
     return (
         <main className=" overflow-x-hidden">
             <MainIntro />
             <MainDesc />
             <MainPosts />
-            <AddressAutoComplete onSelect={handleSelect} />
-            <MapDiv location={location} />
         </main>
     )
 }
