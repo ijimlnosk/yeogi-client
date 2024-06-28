d9115ae2528c9a1a53d5a5096aeb42951b305ccb
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-28 22:10:43 +0900

    YG-101 🟢feat: #open #close #comment login 성공 시 profile 나오게 하기

---
+++ components/layouts/type.ts
@@ -0,0 +1,10 @@
+export type UserInfoProps = {
+    id: number
+    email: string
+    nickname: string
+    ageRange: string
+    gender: "M" | "W"
+    profile: string
+    motto: string
+    banner: string
+}
