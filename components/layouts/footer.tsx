e6833a7c26afd1975ffbee5245e3a8cb8a97c26b
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-06-30 12:57:09 +0900

    YG-104 🎁style: #open #close #comment header, footer 반응형 수정

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -5,29 +5,37 @@ import Image from "next/image"
 
 const Footer = () => {
     return (
-        <footer className={clsx("w-full bg-[#F7EDE0] flex flex-col  px-[120px] py-[80px]")}>
-            <div className={clsx("w-full flex")}>
-                <div className="flex flex-col text-GREY-80 pt-[212px]">
-                    <h2 className="text-bg font-semibold mb-2">About Us</h2>
-                    <p className="text-sm ">
+        <footer className={clsx("w-full bg-[#F7EDE0] flex flex-col  px-[120px] xl:py-[80px]")}>
+            <div className={clsx("w-full grid xl:grid-cols-3 grid-cols-1 ")}>
+                <div className="flex flex-col text-GREY-80 pt-[100px] xl:pt-[212px]">
+                    <h2 className="text-bg font-semibold mb-2 flex justify-center items-center xl:justify-start xl:items-start">
+                        About Us
+                    </h2>
+                    <p className="text-xs flex justify-center items-center xl:justify-start xl:items-start xl:text-sm ">
                         ‘여기’는 비영리 단체인 모비의 지원을 받아 <br /> 개발자와 디자이너가 모여 팀을 이루었습니다.
                         <br /> <br />
                         기존의 인터넷 상에 흩어져 있던 여행 후기들을 <br /> 사용자가 별도의 웹 서칭 없이 한곳에서 확인할
                         수 있는 <br /> 커뮤니티를 제공하고자 서비스를 설계했습니다.
                     </p>
                 </div>
-                <div className="flex flex-col items-center mx-auto font-myeongjo">
+                <div className="flex flex-col items-center mx-auto font-myeongjo pt-10">
                     <Image src={LogoImage} width={120} height={120} className="w-auto h-auto" alt="yeogi logo" />
                     <Image src={LogoText} width={90} height={60} className="w-auto h-auto" alt="yeogi logo" />
                     <p className="text-GREY-50 mt-[16px] text-bg">Record your Trip</p>
                 </div>
-                <div className="font-pretendard flex flex-col text-GREY-50 text-sm place-items-end justify-between mt-auto">
-                    <nav className="flex space-x-8 mb-4">
-                        <a href="#">커뮤니티</a>
-                        <a href="#">기록</a>
-                        <a href="#">마이페이지</a>
+                <div className="font-pretendard flex flex-col text-GREY-50 text-sm items-center py-5 xl:py-0 xl:place-items-end xl:justify-between mt-auto xl:items-center">
+                    <nav className="flex space-x-8 mb-4 w">
+                        <a href="#" className="text-xxs xl:text-sm">
+                            커뮤니티
+                        </a>
+                        <a href="#" className="text-xxs xl:text-sm">
+                            기록
+                        </a>
+                        <a href="#" className="text-xxs xl:text-sm">
+                            마이페이지
+                        </a>
                     </nav>
-                    <p className="text-GREY-80 font-semibold ">© 2024 YEOGI All Rights Reserved.</p>
+                    <p className="text-GREY-80 font-semibold text-xxs xl:text-sm">© 2024 YEOGI All Rights Reserved.</p>
                 </div>
             </div>
         </footer>
