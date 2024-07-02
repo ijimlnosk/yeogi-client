c564548f09a9113730b53ba99977f4c189da7573
hayoung <wendy4332@naver.com>
2024-07-03 00:26:52 +0900

    YG-107 🎁 style : 스켈레톤 스타일 수정

--- components/commons/skeleton.tsx
+++ components/commons/skeleton.tsx
@@ -5,17 +5,17 @@ export const SkeletonCard = () => {
     return (
         <Card className="w-[300px] h-[381px] space-y-5 p-4 rounded-lg">
             <Skeleton className="rounded-lg">
-                <div className="h-24 rounded-lg bg-default-300"></div>
+                <div className="h-24 rounded-lg bg-GREY-70"></div>
             </Skeleton>
             <div className="space-y-3">
                 <Skeleton className="w-3/5 rounded-lg">
-                    <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                    <div className="h-3 w-3/5 rounded-lg bg-GREY-30"></div>
                 </Skeleton>
                 <Skeleton className="w-4/5 rounded-lg">
-                    <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                    <div className="h-3 w-4/5 rounded-lg bg-GREY-30"></div>
                 </Skeleton>
                 <Skeleton className="w-2/5 rounded-lg">
-                    <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+                    <div className="h-3 w-2/5 rounded-lg bg-GREY-70"></div>
                 </Skeleton>
             </div>
         </Card>
