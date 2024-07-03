a26a9339849a49400aab93fdc8c832f39b291934
55555-Jyeon <jyeon380516@gmail.com>
2024-07-04 02:29:16 +0900

    YG-112 🟢 메인페이지 여행 추천 설문조사 홍보 container 생성

---
+++ app/(beforeLogin)/survey/page.tsx
@@ -0,0 +1,3 @@
+export const ForTestDeleteMe = () => {
+    return <div>취향 찾으러 오셨나요?</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 import MainDesc from "./_components/mainDesc"
 import MainIntro from "./_components/mainIntro"
 import MainPosts from "./_components/mainPosts"
+import MainRecommend from "./_components/mainRecommend"
 import MainThemeBanner from "./_components/mainThemeBanner"
 
 const Home = () => {
     return (
         <main className=" overflow-x-hidden">
             <MainIntro />
+            <MainRecommend />
             <MainDesc />
             <MainThemeBanner />
             <MainPosts />
