9695b966a78e121b496580cbbb181f8a78925a0f
55555-Jyeon <jyeon380516@gmail.com>
2024-07-04 23:03:11 +0900

    YG-113 📂 #close #comment sticker 이미지 추가

--- app/(beforeLogin)/survey/page.tsx
+++ app/(beforeLogin)/survey/page.tsx
@@ -1,5 +1,6 @@
 import RecommendPostCard from "./_components/recommendPostCard"
 import { ThemeProps } from "@/app/_components/type"
+import Image from "next/image"
 
 const ForTestDeleteMe = () => {
     const selectedThemes: ThemeProps[] = ["COST_SAVING", "EATING", "HOT_PLACE"] // 설문조사 결과에 따라 선택될 상위 theme들
@@ -7,6 +8,7 @@ const ForTestDeleteMe = () => {
     return (
         <div>
             취향 찾으러 오셨나요?
+            <Image src="/images/sticker.svg" width={140} height={50} alt="스티커" />
             <RecommendPostCard themes={selectedThemes} />
         </div>
     )
