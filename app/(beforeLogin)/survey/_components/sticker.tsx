e606a2a1183e72033c7dea9f1996b044ed9f2251
55555-Jyeon <jyeon380516@gmail.com>
2024-07-05 02:28:47 +0900

    YG-117 🎁 #open #comment post-detail 디자인 수정

---
+++ app/(beforeLogin)/survey/_components/sticker.tsx
@@ -0,0 +1,8 @@
+const Sticker = () => {
+    return (
+        <div className="w-[140px] h-[50px] bg-BRAND-50 text-SYSTEM-white border-8  border-ACCENT-orange flex justify-center items-center">
+            <p className="text-sm font-semibold">Best Fit!</p>
+        </div>
+    )
+}
+export default Sticker
