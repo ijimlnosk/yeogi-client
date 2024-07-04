9d6bc1de58ff335e8ed09f83e33128934a6a8a60
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-07-05 00:31:38 +0900

    YG-110 🎁style: style 수정 완료

--- app/(beforeLogin)/survey/_components/surveyForm/surveyQuestionForm.tsx
+++ app/(beforeLogin)/survey/_components/surveyForm/surveyQuestionForm.tsx
@@ -12,7 +12,7 @@ const SurveyQuestionForm = () => {
     if (showResults) {
         return (
             <div className="w-[1050px] h-[750px] flex flex-col items-center overflow-hidden relative">
-                <div className="w-[730px] h-[494px] bg-SYSTEM-white rounded-[24px] p-4 flex flex-col justify-center gap-2 items-center pt-16">
+                <div className="w-[730px] h-[494px] bg-SYSTEM-white rounded-3xl p-4 flex flex-col justify-center gap-2 items-center pt-16">
                     <h2 className="font-semibold text-xl pb-6">결과</h2>
                     {topTags.map(tag => (
                         <p key={tag} className="w-[530px] text-lg">
