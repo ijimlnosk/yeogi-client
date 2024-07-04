649d70987f8b3ce1c75ba203f1e8c5ea07cca8e7
55555-Jyeon <jyeon380516@gmail.com>
2024-07-04 22:24:42 +0900

    YG-113 🟢 #open #comment 테마별 추천 postcard 컴포넌트

---
+++ app/(beforeLogin)/survey/_components/type.ts
@@ -0,0 +1,3 @@
+export type RecommendPostCardProps = {
+    themes: string[]
+}
---
+++ constants/recommendedTextFields.ts
@@ -0,0 +1,49 @@
+import { RecommendedTextFieldsType } from "./type"
+
+export const RecommendedTextFields: RecommendedTextFieldsType[] = [
+    {
+        theme: "REST",
+        title: "",
+        travelType: "휴식 여행",
+    },
+    {
+        theme: "EATING",
+        title: "금강산도 식후경!",
+        travelType: "맛집 여행",
+    },
+    {
+        theme: "HOT_PLACE",
+        title: "",
+        travelType: "인기명소 여행",
+    },
+    {
+        theme: "SHOPPING",
+        title: "",
+        travelType: "쇼핑 여행",
+    },
+    {
+        theme: "ACTIVITY",
+        title: "지루한건 질색!",
+        travelType: "액티비티 여행",
+    },
+    {
+        theme: "SIGHTSEEING",
+        title: "",
+        travelType: "관광명소 여행",
+    },
+    {
+        theme: "PACKAGE",
+        title: "",
+        travelType: "패키지 여행",
+    },
+    {
+        theme: "LUXURY",
+        title: "",
+        travelType: "초호화 여행",
+    },
+    {
+        theme: "COST_SAVING",
+        title: "내 통장을 지켜줘!",
+        travelType: "가성비 여행",
+    },
+]
