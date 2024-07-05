75910cfa0bcdc4f773b863dd2fd240d5651e8228
hayoung <wendy4332@naver.com>
2024-07-06 01:50:35 +0900

    YG-118 🟢 feat : headerNavigate path로 관리

--- components/layouts/_components/headerNavigate.tsx
+++ components/layouts/_components/headerNavigate.tsx
@@ -1,29 +1,28 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 const HeaderNavigate = () => {
-    const [activePage, setActivePage] = useState<string>("/")
     const router = useRouter()
+    const pathName = usePathname()
 
     const handleClick = (page: string) => {
         router.push(page)
-        setActivePage(page)
     }
 
     return (
         <nav className="hidden md:block">
-            <ul className="w-full h-20  ml-8 flex justify-evenly items-center">
+            <ul className="w-full h-20  ml-8 flex justify-evenly items-center cursor-pointer">
                 <li
                     onClick={() => handleClick("/")}
-                    className={`text-SYSTEM-black font-bold text-xs xl:text-sm  w-fit px-6 py-8 ${activePage === "/" ? "border-b-2 border-SYSTEM-black" : ""}`}
+                    className={`text-SYSTEM-black  font-bold text-xs xl:text-sm  w-fit px-6 py-8 ${pathName === "/" ? "border-b-2 border-SYSTEM-black" : ""}`}
                 >
                     커뮤니티
                 </li>
                 <li
                     onClick={() => handleClick("/survey")}
-                    className={`text-SYSTEM-black font-bold text-xs xl:text-sm  w-fit px-6 py-8 ${activePage === "/survey" ? "border-b-2 border-SYSTEM-black" : ""}`}
+                    className={`text-SYSTEM-black font-bold text-xs xl:text-sm  w-fit px-6 py-8 ${pathName === "/survey" ? "border-b-2 border-SYSTEM-black" : ""}`}
                 >
                     내 취향 찾기
                 </li>
