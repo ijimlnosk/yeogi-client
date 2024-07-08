580cbfdd9e1a7feb334b652498e2cb4b573d7049
hayoung <wendy4332@naver.com>
2024-07-08 18:31:58 +0900

    YG-121 💡 refactor : 새로고침시, 프로필 변경되는점 수정

--- components/layouts/_components/headerLogin.tsx
+++ components/layouts/_components/headerLogin.tsx
@@ -1,16 +1,24 @@
-"use Client"
+"use client"
 
 import Link from "next/link"
 import Image from "next/image"
 import ProtectedLink from "@/components/protectedLink"
-import { HeaderLoginProps } from "./type"
 import { useEffect, useState } from "react"
+import { useLoggedIn } from "@/libs/loginStore"
 
-const HeaderLogin = ({ isLoggedIn, userInfo }: HeaderLoginProps) => {
-    const [profileImage, setProfileImage] = useState<string>("")
+const HeaderLogin = () => {
+    const [profileImage, setProfileImage] = useState<string>("/images/sampleProfile.svg")
+    const [isImageLoading, setIsImageLoading] = useState(true)
+    const { isLoggedIn, userInfo } = useLoggedIn()
 
-    console.log("isLoggedIn:", isLoggedIn)
-    console.log("userInfo:", userInfo)
+    useEffect(() => {
+        if (userInfo) {
+            setIsImageLoading(true)
+            const image = getUserProfileImage()
+            setProfileImage(image)
+        }
+    }, [userInfo])
+
     const getUserProfileImage = (): string => {
         if (userInfo?.profile) {
             return userInfo.profile
@@ -20,33 +28,29 @@ const HeaderLogin = ({ isLoggedIn, userInfo }: HeaderLoginProps) => {
         }
         return "/images/sampleProfile.svg"
     }
-    useEffect(() => {
-        if (userInfo) {
-            setProfileImage(getUserProfileImage())
-        }
-    }, [userInfo])
+
+    const handleImageLoad = () => {
+        setIsImageLoading(false)
+    }
+
     return (
         <>
             {isLoggedIn ? (
-                <Link href={`/user/${userInfo?.id}`}>
-                    {userInfo?.profile || userInfo?.profile_image ? (
+                <Link href={`/user/${userInfo?.id}`} className="w-12 h-12 relative">
+                    {isImageLoading && (
+                        <div className="absolute inset-0 flex items-center justify-center bg-gray-200 rounded-full">
+                            <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900"></div>
+                        </div>
+                    )}
+                    {profileImage && (
                         <Image
-                            src={getUserProfileImage()}
+                            src={profileImage}
                             alt="profile"
                             width={48}
                             height={48}
-                            className="rounded-full"
+                            className={`rounded-full ${isImageLoading ? "invisible" : "visible"}`}
+                            onLoadingComplete={handleImageLoad}
                         />
-                    ) : (
-                        <div>
-                            <Image
-                                src={"/images/sampleProfile.svg"}
-                                alt="Profile"
-                                width={48}
-                                height={48}
-                                className="rounded-full"
-                            />
-                        </div>
                     )}
                 </Link>
             ) : (
@@ -57,4 +61,5 @@ const HeaderLogin = ({ isLoggedIn, userInfo }: HeaderLoginProps) => {
         </>
     )
 }
+
 export default HeaderLogin
