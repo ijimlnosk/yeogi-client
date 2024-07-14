3fec4c6abbb4166efc43d110fceaf2db90f3705a
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-07-14 13:21:50 +0900

    YG-130 💡refactor: #close #comment 불필요한 로직 제거 및 위치 변경

--- components/commons/rankCard.tsx
+++ components/commons/rankCard.tsx
@@ -2,12 +2,7 @@ import { formatISODateString } from "@/app/(afterLogin)/detailPost/[postId]/date
 import { Post } from "@/types/post"
 import Image from "next/image"
 import Link from "next/link"
-
-export type RankCardProps = {
-    topPosts: Post
-    rank: "Top1" | "Top2" | "Top3"
-    topPostId: number
-}
+import { RankCardProps } from "./type"
 
 const RankCard = ({ topPosts, rank, topPostId }: RankCardProps) => {
     const getTopStyle = () => {
@@ -24,52 +19,50 @@ const RankCard = ({ topPosts, rank, topPostId }: RankCardProps) => {
     }
 
     return (
-        <>
-            <Link
-                href={`/detailPost/${topPostId}`}
-                className="w-[506px] h-[400px] relative bg-SYSTEM-white rounded-3xl p-4 overflow-hidden cursor-pointer group"
+        <Link
+            href={`/detailPost/${topPostId}`}
+            className="w-[506px] h-[400px] relative bg-SYSTEM-white rounded-3xl p-4 overflow-hidden cursor-pointer group"
+        >
+            <div className=" absolute w-full h-full top-0 left-0 bg-SYSTEM-black">
+                <Image
+                    src={"/images/rankCardThumbnail.svg"}
+                    alt="thumbnail"
+                    width={506}
+                    height={400}
+                    className=" absolute top-0 left-0 opacity-70 group-hover:opacity-30 transition-opacity duration-300 "
+                />
+            </div>
+            <div className=" absolute flex flex-row ">
+                <Image src={"/icons/white_gps.svg"} alt="gps" width={16} height={16} />
+                <p className="pl-1 text-SYSTEM-white text-sm font-semibold">{topPosts.region}</p>
+            </div>
+            <div
+                className={`absolute right-4 w-[58px] h-[38px] rounded-lg flex justify-center items-center ${getTopStyle()}`}
             >
-                <div className=" absolute w-full h-full top-0 left-0 bg-SYSTEM-black">
-                    <Image
-                        src={"/images/rankCardThumbnail.svg"}
-                        alt="thumbnail"
-                        width={506}
-                        height={400}
-                        className=" absolute top-0 left-0 opacity-70 group-hover:opacity-30 transition-opacity duration-300 "
-                    />
+                <p className=" text-SYSTEM-white">{rank}</p>
+            </div>
+            <div className=" absolute w-[458px] h-[99px] bottom-4 right-6">
+                <div>
+                    <p className=" text-SYSTEM-white text-md">{topPosts.title}</p>
+                    <p className=" text-GREY-30 text-xxs">게시일: {formatISODateString(topPosts.createdAt)}</p>
                 </div>
-                <div className=" absolute flex flex-row ">
-                    <Image src={"/icons/white_gps.svg"} alt="gps" width={16} height={16} />
-                    <p className="pl-1 text-SYSTEM-white text-sm font-semibold">{topPosts.region}</p>
-                </div>
-                <div
-                    className={`absolute right-4 w-[58px] h-[38px] rounded-lg flex justify-center items-center ${getTopStyle()}`}
-                >
-                    <p className=" text-SYSTEM-white">{rank}</p>
-                </div>
-                <div className=" absolute w-[458px] h-[99px] bottom-4 right-6">
+                <div className="flex justify-between">
+                    <div className="flex flex-row gap-3 pt-6">
+                        <div className="flex flex-row">
+                            <Image src={"/icons/comment.svg"} alt="comment" width={16} height={16} />
+                            <p className="pl-1 text-GREY-30">{topPosts.commentCount}</p>
+                        </div>
+                        <div className="flex flex-row">
+                            <Image src={"/icons/like.svg"} alt="comment" width={16} height={16} />
+                            <p className="pl-1 text-GREY-30">{topPosts.likeCount}</p>
+                        </div>
+                    </div>
                     <div>
-                        <p className=" text-SYSTEM-white text-md">{topPosts.title}</p>
-                        <p className=" text-GREY-30 text-xxs">게시일: {formatISODateString(topPosts.createdAt)}</p>
-                    </div>
-                    <div className="flex justify-between">
-                        <div className="flex flex-row gap-3 pt-6">
-                            <div className="flex flex-row">
-                                <Image src={"/icons/comment.svg"} alt="comment" width={16} height={16} />
-                                <p className="pl-1 text-GREY-30">{topPosts.commentCount}</p>
-                            </div>
-                            <div className="flex flex-row">
-                                <Image src={"/icons/like.svg"} alt="comment" width={16} height={16} />
-                                <p className="pl-1 text-GREY-30">{topPosts.likeCount}</p>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="pt-6 text-GREY-30">{topPosts.author}</div>
-                        </div>
+                        <div className="pt-6 text-GREY-30">{topPosts.author}</div>
                     </div>
                 </div>
-            </Link>
-        </>
+            </div>
+        </Link>
     )
 }
 export default RankCard
