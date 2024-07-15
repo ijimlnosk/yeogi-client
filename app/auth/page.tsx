5366bdd6b2bbaaeffe4382ada351a9ac4ab40d18
hayoung <wendy4332@naver.com>
2024-07-15 15:41:01 +0900

    YG-138 💡refactor : socialLoginLayout 분리

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,20 +1,17 @@
 "use client"
 
-import kakaoIcon from "@/public/icons/kakao.svg"
-import naverIcon from "@/public/icons/naver_icon 1.svg"
-import googleIcon from "@/public/icons/google.svg"
-import SocialLoginButton from "./_components/signin/socialLoginButton"
 import { useEffect, useState } from "react"
 import { getCodeFromUrl, postAuthCode } from "@/apis/auth/oauthApi"
 import { setCookieToken } from "@/apis/auth/storageUtils"
 import { useRouter } from "next/navigation"
+import SocialLoginLayout from "./_components/signin/socialLoginLayout"
 
 type Provider = "kakao" | "google" | "naver"
 const AuthForm = () => {
     const K_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API
     const G_REST_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_REST_API
     const N_REST_API_KEY = process.env.NEXT_PUBLIC_NAVER_REST_API
-    // const REDIRECT_URI = `http://localhost:3000/auth`
+    //const REDIRECT_URI = `http://localhost:3000/auth`
     const REDIRECT_URI = `https://yeogi-client.vercel.app/auth`
     const [provider, setProvider] = useState<string | null>(null)
 
@@ -47,42 +44,12 @@ const AuthForm = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className=" flex flex-col  justify-center w-[496px] h-[454px] top-[308px] left-[712px] rounded-2xl  bg-SYSTEM-else p-12 ">
-                <div>
-                    <div className="flex justify-center h-[64px] text-[44px] text-BRAND-70 font-myeongjo items-center">
-                        Login
-                    </div>
-                    <div className=" flex justify-center text-center pt-10 pb-8">
-                        <div className="flex items-center w-full ">
-                            <div className="flex-grow h-[1px] bg-BRAND-30"></div>
-                            <div className="flex-shrink-0 px-[10px]  text-BRAND-70 font-myeongjo text-xs text-center justify-center items-center">
-                                SNS로 간편하게 로그인하세요.
-                            </div>
-                            <div className="flex-grow h-[1px] bg-BRAND-30"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-4">
-                    <SocialLoginButton
-                        icon={kakaoIcon}
-                        text={"카카오톡으로 간편하게 로그인"}
-                        bgColor="bg-SNS-kakao"
-                        onClick={() => handleMoveSocialLogin(kakaoURL, "kakao")}
-                    />
-                    <SocialLoginButton
-                        icon={naverIcon}
-                        text={"네이버로 간편하게 로그인"}
-                        bgColor="bg-SNS-naver"
-                        onClick={() => handleMoveSocialLogin(naverURL, "naver")}
-                    />
-                    <SocialLoginButton
-                        icon={googleIcon}
-                        text={"GOOGLE로 간편하게 로그인"}
-                        bgColor="bg-SYSTEM-white"
-                        onClick={() => handleMoveSocialLogin(googleURL, "google")}
-                    />
-                </div>
-            </div>
+            <SocialLoginLayout
+                kakaoURL={kakaoURL}
+                naverURL={naverURL}
+                googleURL={googleURL}
+                handleMoveSocialLogin={handleMoveSocialLogin}
+            />
         </div>
     )
 }
