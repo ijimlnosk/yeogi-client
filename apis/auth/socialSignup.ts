3fada2451f4e34c07a5e8ae29ff265e7a465da0c
hayoung <wendy4332@naver.com>
2024-07-18 19:11:47 +0900

    YG-138 🟢feat : addInfoForm생성 및 signup기능 생성

--- apis/auth/socialSignup.ts
+++ apis/auth/socialSignup.ts
@@ -8,9 +8,6 @@ export const postSocialSignup = async (data: SocialSignupRequest) => {
             method: "POST",
             body: JSON.stringify(data),
         })
-        if (!response.ok) {
-            throw new Error("회원가입 요청에 실패하였습니다.")
-        }
         const user = await response.json()
         return user
     } catch (error) {
@@ -18,18 +15,10 @@ export const postSocialSignup = async (data: SocialSignupRequest) => {
     }
 }
 
-export const postMemberCheckExists = async (data: SocialSignupCheckExists) => {
-    try {
-        const response = await fetchFormAPINotToken(`${BASE_URL}`, "member/checkExists", {
-            method: "POST",
-            body: JSON.stringify(data),
-        })
-        if (!response.ok) {
-            throw new Error("이미 있는 회원입니다")
-        }
-        const user = await response.json()
-        return user
-    } catch (error) {
-        throw new Error("서버와의 통신 중 오류가 발생하였습니다.")
-    }
+export const postMemberCheckExists = async (nickname: string) => {
+    const response = await fetchFormAPINotToken(`${BASE_URL}`, `member/nicknames?nickname=${nickname}`, {
+        method: "GET",
+    })
+    const user = await response.json()
+    return user
 }
