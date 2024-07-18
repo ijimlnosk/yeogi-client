cf1619ceb632234d982cdf78c4e89c8957211239
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-07-18 15:10:34 +0900

    YG-144 💡refactor: search modal 외부 스크롤 안되게 수정

--- app/_components/mainSearch.tsx
+++ app/_components/mainSearch.tsx
@@ -41,14 +41,26 @@ const MainSearch = () => {
     }, [])
 
     useEffect(() => {
+        if (isSearchOpen) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "unset"
+        }
+
+        return () => {
+            document.body.style.overflow = "unset"
+        }
+    }, [isSearchOpen])
+
+    useEffect(() => {
         refetch()
     }, [refetch, searchKeyword, sortCondition])
 
     if (!isSearchOpen) return null
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-0 flex items-center justify-center z-[1000] pt-10">
-            <div className="w-[98%] max-w-screen h-[90%] max-h-screen flex flex-col bg-MAIN_SEARCH bg-center bg-cover bg-no-repeat rounded-3xl xl:rounded-2xl transition-all duration-[1000ms] ease-in-out delay-75">
+        <div className="fixed inset-0 bg-black bg-opacity-0 flex items-center justify-center z-[1000] pt-20">
+            <div className="w-[98%] max-w-screen h-[96%] max-h-screen flex flex-col bg-MAIN_SEARCH bg-center bg-cover bg-no-repeat rounded-3xl xl:rounded-2xl transition-all duration-[1000ms] ease-in-out delay-75">
                 <button
                     className="self-end m-4 text-white text-2xl w-10 h-10 mt-4 mr-4"
                     onClick={() => setIsSearchOpen(false)}
@@ -56,7 +68,7 @@ const MainSearch = () => {
                     <Image src={"/icons/x-circle.svg"} alt="x-circle" width={40} height={40} />
                 </button>
                 <div
-                    className={`h-full flex justify-center items-center flex-col transition-all duration-300 ${isFocused ? "pb-[640px]" : "pb-0"}`}
+                    className={`h-full flex justify-center items-center flex-col transition-all duration-300 ${isFocused ? "pb-[640px]" : "pb-40"}`}
                 >
                     <div
                         className={`w-full flex flex-col items-center py-12 transition-transform duration-[300ms] ease-in-out
