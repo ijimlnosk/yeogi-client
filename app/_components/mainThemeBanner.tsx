76b22943c4feef7c3806f688a2e854c7a6e31ee0
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-07-20 17:27:54 +0900

    YG-153 🐞fix: #close #comment build error 해결

--- app/_components/mainThemeBanner.tsx
+++ app/_components/mainThemeBanner.tsx
@@ -4,7 +4,7 @@ import { ThemeBannerContext } from "@/constants/themeBannerTexts"
 import { Theme } from "@/types/theme"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { useEffect, useRef } from "react"
+import { useRef } from "react"
 
 const MainThemeBanner = () => {
     const router = useRouter()
