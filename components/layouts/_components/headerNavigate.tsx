24fc29a82b1592833619f60b132e5deff808ab3e
55555-Jyeon <jyeon380516@gmail.com>
2024-07-23 01:03:54 +0900

    YG-151 💡 header 네비게이션 리스트 분리

--- components/layouts/_components/headerNavigate.tsx
+++ components/layouts/_components/headerNavigate.tsx
@@ -1,28 +1,22 @@
+import { NavigateList } from "@/constants/navigateList"
 import { usePathname, useRouter } from "next/navigation"
 
 const HeaderNavigate = () => {
     const router = useRouter()
     const pathName = usePathname()
 
-    const handleClick = (page: string) => {
-        router.push(page)
-    }
-
     return (
         <nav className="hidden md:block">
-            <ul className="w-full h-20 ml-8 md:ml-4 flex justify-evenly items-center cursor-pointer">
-                <li
-                    onClick={() => handleClick("/")}
-                    className={`text-SYSTEM-black font-bold text-xs xl:text-sm w-fit px-6 py-8 md:px-3 ${pathName === "/" ? "border-b-2 border-SYSTEM-black" : ""}`}
-                >
-                    커뮤니티
-                </li>
-                <li
-                    onClick={() => handleClick("/survey")}
-                    className={`text-SYSTEM-black font-bold text-xs xl:text-sm w-fit px-6 py-8 md:px-3 ${pathName === "/survey" ? "border-b-2 border-SYSTEM-black" : ""}`}
-                >
-                    내 취향 찾기
-                </li>
+            <ul className="w-full h-20 2xl:ml-8 md:ml-4 flex justify-evenly items-center cursor-pointer">
+                {NavigateList.map(({ title, path }) => (
+                    <li
+                        key={path}
+                        onClick={() => router.push(path)}
+                        className={`text-SYSTEM-black font-bold text-xs xl:text-sm w-fit px-6 py-8 md:px-3 ${pathName === path ? "border-b-2 border-SYSTEM-black" : ""}`}
+                    >
+                        {title}
+                    </li>
+                ))}
             </ul>
         </nav>
     )
