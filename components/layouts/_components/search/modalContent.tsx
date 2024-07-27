914e802e79fecd36fc0028b5903d93d5707f31c5
55555-Jyeon <jyeon380516@gmail.com>
2024-07-27 21:36:41 +0900

    YG-166 🎁 #close #comment 스타일 변경

--- components/layouts/_components/search/modalContent.tsx
+++ components/layouts/_components/search/modalContent.tsx
@@ -8,8 +8,6 @@ import { modalContentProps } from "./type"
 
 const ModalContent = ({ handleCloseSearch, debouncedSearchKeyword }: modalContentProps) => {
     const [isFocused, setIsFocused] = useState<boolean>(false)
-    const containerStyle = `h-full flex justify-center items-center flex-col transition-all duration-300 ${isFocused ? "pb-[640px]" : "pb-40"}`
-    const titleContainerStyle = `w-full flex flex-col items-center py-12 transition-transform duration-[300ms] ease-in-out ${isFocused ? "-translate-y-1" : "translate-y-0"}`
 
     return (
         <div className="w-full max-w-screen h-full max-h-screen flex flex-col bg-MAIN_SEARCH bg-center bg-cover bg-no-repeat rounded-3xl xl:rounded-2xl transition-all duration-[1000ms] ease-in-out delay-75">
@@ -21,8 +19,12 @@ const ModalContent = ({ handleCloseSearch, debouncedSearchKeyword }: modalConten
                 onClick={handleCloseSearch}
                 className="self-end m-4"
             />
-            <div className={containerStyle}>
-                <ModalTitleSection titleContainerStyle={titleContainerStyle} />
+            <div
+                className={`h-full flex justify-center items-center flex-col transition-all duration-300 ${isFocused ? "pb-[640px]" : "pb-40"}`}
+            >
+                <ModalTitleSection
+                    titleContainerStyle={`w-full flex flex-col items-center py-12 transition-transform duration-[300ms] ease-in-out ${isFocused ? "-translate-y-1" : "translate-y-0"}`}
+                />
                 <SearchBar
                     onChange={e => debouncedSearchKeyword(e.target.value)}
                     text="찾고 싶은 여행 기록을 검색하세요."
