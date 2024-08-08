c57d3e24daf05cbf434b0bcbebba49e7089d31c7
hayoung <wendy4332@naver.com>
2024-08-08 12:16:33 +0900

    YG-178 💡refactor: redirecturi 수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,15 +3,14 @@ import GoogleProvider from "next-auth/providers/google"
 import Kakao from "next-auth/providers/kakao"
 import Naver from "next-auth/providers/naver"
 
-const G_REST_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_REST_API
-const G_CLIENT_SECRET_KEY = process.env.NEXT_PUBLIC_GOOGLE_ADMIN_KEY
+const G_REST_API_KEY = process.env.GOOGLE_CLIENT_ID
+const G_CLIENT_SECRET_KEY = process.env.GOOGLE_CLIENT_SECRET
 const K_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API
 const K_CLIENT_SECRET_KEY = process.env.NEXTAUTH_KAKAO_ADMIN_KEY
 const N_REST_API_KEY = process.env.NEXT_PUBLIC_NAVER_REST_API
 const N_CLIENT_SECRET_KEY = process.env.NEXT_PUBLIC_NAVER_ADMIN_KEY
 const AUTH_API_URL = "http://ec2-43-203-193-158.ap-northeast-2.compute.amazonaws.com:8080"
-const redirect_uri = "http://localhost:3000/auth"
-//const redirect_uri = "https://yeogi-client.vercel.app/auth"
+const redirect_uri = "http://localhost:3000/api/auth/callback"
 
 declare module "next-auth" {
     interface Session {
@@ -28,7 +27,8 @@ declare module "next-auth/jwt" {
 }
 
 const handler = NextAuth({
-    secret: process.env.NEXTAUTH_SECRET, // secret: "http://localhost:3000",
+    debug: true,
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: G_REST_API_KEY || "",
@@ -50,34 +50,36 @@ const handler = NextAuth({
     },
     callbacks: {
         async jwt({ token, account }) {
-            console.log("fetch start")
-            console.log("JWT Callback - Account:", account)
-            console.log("JWT Callback - Initial Token:", token)
-
-            if (account?.provider && account?.access_token) {
+            if (account && account?.access_token) {
                 console.log("실행되나?")
-                const authorizationParams = new URLSearchParams(window.location.search)
-                const authorizationCode = authorizationParams.get("code")
-                console.log(authorizationCode)
-                if (authorizationCode) {
-                    const response = await fetch(
-                        //디코채팅 보슈
-                        `${AUTH_API_URL}/auth/generateToken/${account.provider}?code=${authorizationCode}&redirect_uri=${redirect_uri}`,
-                        { method: "POST" },
-                    )
-                    if (response.ok) {
-                        const data = await response.json()
-                        console.log(data, "BE response data")
-                        token.accessToken = data.accessToken // 백엔드에서 받은 토큰 저장
-                        token.refreshToken = data.refreshToken // 리프레시 토큰이 있다면 저장
-                        token.accessTokenExpires = Date.now() + data.expiresIn * 1000
-                        token.accessToken = data.accessToken
-                    } else {
-                        console.error("failed to fetch token from BE", response.status)
-                    }
-                    // 첫 로그인 여부를 확인하는 로직 추가 가능
+                //여기가 백엔드 쪽 연결이 안된다.
+                const response = await fetch(
+                    `${AUTH_API_URL}/${account.provider}`, //백엔드 주소 + provider(google) 인가코드는 아니야
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            access_token: account.access_token,
+                            refresh_token: account.refresh_token,
+                            id_token: account.id_token,
+                            redirect_uri: `${redirect_uri}/${account.provider}`,
+                        }),
+                    },
+                )
+                // http://ec2-43-203-193-158.ap-northeast-2.compute.amazonaws.com:8080/google
+                if (response.ok) {
+                    const data = await response.json()
+                    console.log(data, "BE response data")
+                    token.accessToken = data.accessToken // 백엔드에서 받은 토큰 저장
+                    token.refreshToken = data.refreshToken // 리프레시 토큰이 있다면 저장
+                } else {
+                    console.error("failed to fetch token from BE", response.status)
                 }
+                // 첫 로그인 여부를 확인하는 로직 추가 가능
             }
+            // }
             console.log(token, "JWT token")
             return token
         },
