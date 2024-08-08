f58e89bf66844881e8f3443a36ec772ba645096a
55555-Jyeon <jyeon380516@gmail.com>
2024-08-08 15:11:10 +0900

    YG-187 🔴 취향 수정 버튼 삭제

--- app/(afterLogin)/user/[userId]/_components/profile/profileDetails.tsx
+++ app/(afterLogin)/user/[userId]/_components/profile/profileDetails.tsx
@@ -1,11 +1,6 @@
-"use client"
-
-import StillWorkingOverlay from "@/components/commons/stillWorkingOverlay"
 import { ProfileDetailsProps } from "./type"
-import { useState } from "react"
 
 const ProfileDetails = ({ ageRange, gender }: ProfileDetailsProps) => {
-    const [isInProgress, setIsInProgress] = useState<boolean>(false)
     const displayGender = (gender: string | undefined) => {
         if (gender === "M") return "남성"
         if (gender === "F") return "여성"
@@ -13,27 +8,18 @@ const ProfileDetails = ({ ageRange, gender }: ProfileDetailsProps) => {
     }
 
     return (
-        <>
-            <div className="flex justify-end mr-[120px] mt-10 mb-[120px]">
-                <div className="flex text-bg">
-                    <button
-                        onClick={() => setIsInProgress(true)}
-                        className="bg-SYSTEM-white p-5 mr-6 rounded-2xl w-24 h-[120px] text-center"
-                    >
-                        취향 수정
-                    </button>
-                    <span className="bg-SYSTEM-white p-5 mr-6 rounded-2xl w-24 h-[120px] text-center">
-                        {ageRange && <span>{ageRange}&nbsp;대</span>}
-                        {gender && <span>{displayGender(gender)}</span>}
-                    </span>
-                    <span className="bg-SYSTEM-white p-5 rounded-2xl w-[156px] h-[120px] text-center">
-                        나의 기록 핀 <br />
-                        <span className="text-BRAND-50 font-semibold">준비 중</span>
-                    </span>
-                </div>
+        <div className="flex justify-end mr-[120px] mt-10 mb-[120px]">
+            <div className="flex text-bg">
+                <span className="bg-SYSTEM-white p-5 mr-6 rounded-2xl w-24 h-[120px] text-center">
+                    {ageRange && <span>{ageRange}&nbsp;대</span>}
+                    {gender && <span>{displayGender(gender)}</span>}
+                </span>
+                <span className="bg-SYSTEM-white p-5 rounded-2xl w-[156px] h-[120px] text-center">
+                    나의 기록 핀 <br />
+                    <span className="text-BRAND-50 font-semibold">준비 중</span>
+                </span>
             </div>
-            <StillWorkingOverlay isOpen={isInProgress} onClick={() => setIsInProgress(false)} />
-        </>
+        </div>
     )
 }
 
