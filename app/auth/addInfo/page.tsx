333a59544d3d3fb51611351d1663730304714bbc
hayoung <wendy4332@naver.com>
2024-08-16 18:10:02 +0900

    YG-178 💡refactor : 쿠키 재설정

--- app/auth/addInfo/page.tsx
+++ app/auth/addInfo/page.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react"
 import AddInfoForm from "../_components/signup/addInfoForm"
 import { Session } from "next-auth"
 import { isNull } from "@/utils/typeGuard"
+import { getCookie } from "cookies-next"
 
 const Page = () => {
     const { data: session } = useSession()
@@ -14,21 +15,6 @@ const Page = () => {
 }
 
 export default Page
-
-export function getCookie(name: string): string | null {
-    if (typeof window !== "undefined") {
-        const value = `; ${document.cookie}`
-        const parts = value.split(`; ${name}=`)
-
-        if (parts.length === 2) {
-            const cookiePart = parts.pop()
-            if (cookiePart) {
-                return cookiePart.split(";").shift() || null
-            }
-        }
-    }
-    return null
-}
 
 const changeSessionToFormData = (session: Session) => {
     const userData = {
