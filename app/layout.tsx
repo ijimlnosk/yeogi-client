5b80dbee3a4e5f650975f8741b52204143dbe8f0
55555-Jyeon <jyeon380516@gmail.com>
2024-08-18 23:52:32 +0900

    YG-182 🔖 layout에서 font 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,10 @@
 import type { Metadata } from "next"
-import { Nanum_Myeongjo } from "next/font/google"
-import localFont from "next/font/local"
 import "./globals.css"
 import Layout from "@/components/layouts/layout"
 import { ReactQueryProvider } from "@/components/queryProvider"
 import ClientLayout from "./clientLayout"
 import { NextAuthSession } from "./nextAuthSession"
-
-const myeongjo = Nanum_Myeongjo({ weight: ["400", "700"], subsets: ["latin"], variable: "--font-myeongjo" })
-const pretendard = localFont({
-    src: [
-        {
-            path: "../public/fonts/Pretendard-Bold.woff",
-            weight: "700",
-            style: "normal",
-        },
-        {
-            path: "../public/fonts/Pretendard-SemiBold.woff",
-            weight: "600",
-            style: "normal",
-        },
-        {
-            path: "../public/fonts/Pretendard-Medium.woff",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../public/fonts/Pretendard-Regular.woff",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../public/fonts/Pretendard-Light.woff",
-            weight: "300",
-            style: "normal",
-        },
-    ],
-    variable: "--font-pretendard",
-})
+import { myeongjo, pretendard } from "./fonts"
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
@@ -67,7 +34,6 @@ export default function RootLayout({
                     defer
                 />
             </head>
-
             <body className={`${myeongjo.variable} ${pretendard.variable}`}>
                 <NextAuthSession>
                     <ReactQueryProvider>
